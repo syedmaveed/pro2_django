1f8573f37d7f9e186888bff95b09589683c6cc99
Syed Maveed <maveed.kazi@yahoo.com>
2022-08-12 21:38:02 +0530

    updated models.py initial

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+#connect post with post admin
+
+admin.site.register(Post,PostAdmin)
